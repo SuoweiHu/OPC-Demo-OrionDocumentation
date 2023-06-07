60daf36f4d7f7f8cfdc51157c3087f35a03cbeaa
Suowei Hu <suowei.h@gmail.com>
2023-06-07 14:18:47 +1000

    changing configuration for deploying

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,6 +5,8 @@
 
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const organizationName = "SuoweiHu"
+const projectName = "OPC-Orion-Documentation"
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -13,17 +15,17 @@ const config = {
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://SuoweiHu.github.io/OPC-Orion-Documentation",
+	url: "https://"+organizationName+".github.io",
 
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+	baseUrl: "/"+projectName,
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "SuoweiHu", // Usually your GitHub org/user name.
-	projectName: "OPC-Orion-Documentation", // Usually your repo name.
-    trailingSlash: false,
+	organizationName: organizationName, // Usually your GitHub org/user name.
+	projectName: projectName, // Usually your repo name.
+    // trailingSlash: false,
 
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
